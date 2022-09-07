b0f040395b96a87839d75f46df6215c93c73cd3b
Alexsander Loula <alex.loula@gmail.com>
2022-09-07 13:34:54 -0300

    fix images

--- src/Components/MainPage/index.js
+++ src/Components/MainPage/index.js
@@ -29,7 +29,7 @@ const photos = [
   { src: '/images/jobs.jpg' },
   { src: '/images/marlon_brando.jpg' },
   { src: '/images/mr_bean.jpg' },
-  { src: '/images/ned.jpeg
+  { src: '/images/ned.jpeg' },
   { src: '/images/obama.jpg' },
   { src: '/images/o_iluminado.jpg' },
   { src: '/images/oldie.png' },
